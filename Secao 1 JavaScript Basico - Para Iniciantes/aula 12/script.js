21daed741b5fcf5a5bec001198d7df527c8bbfd5
MateusMarquessz <mateusmarques53@outlook.com>
2024-02-06 15:47:50 -0300

    exercicio

--- Secao 1 JavaScript Basico - Para Iniciantes/aula 12/script.js
+++ Secao 1 JavaScript Basico - Para Iniciantes/aula 12/script.js
@@ -1,1 +1,10 @@
 alert('Mensagem super foda');
+
+window.confirm('deseja apagar');
+//retorna boolean!
+
+window.prompt("digite seu nome");
+
+//
+const confirma = confirm('Realmente deseja apagar?')
+//agora essa varivel tem um valor
