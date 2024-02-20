43c24dcd69531373bd927ddca0b61fcf0ee4ba62
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-02-20 15:47:35 -0300

    exercicios

--- Secao 1 JavaScript Basico - Para Iniciantes/aula 20/tests/script.js
+++ Secao 1 JavaScript Basico - Para Iniciantes/aula 20/tests/script.js
@@ -18,14 +18,22 @@ function meuEscopo(){
     function recebeEventoForm(evento){
         evento.preventDefault();
 
-        const nome = form.querySelector('.nome');
-        const sobrenome = form.querySelector('.sobrenome');
-        const peso = form.querySelector('.peso');
-        const altura = form.querySelector('.altura');
+        const nome = form.querySelector('.Nome');
+        const sobrenome = form.querySelector('.Sobrenome');
+        const peso = form.querySelector('.Peso');
+        const altura = form.querySelector('.Altura');
     
-        
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+                
 
         console.log(pessoas);
+
+        resultado.innerHTML += `<p> ${nome.value} ${sobrenome.value}` + `${peso.value} ${altura.value}</p>`
     }
     form.addEventListener('submit',recebeEventoForm);
 }
