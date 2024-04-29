8d552da52ea5ff2bbacfb00efab18f82b332f7dd
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-04-29 15:47:44 -0300

    estudos

---
+++ Secao 2 JavaScript - Logica de Programacao/aula 39/index.js
@@ -0,0 +1,13 @@
+// try  um teste
+
+try{
+    console.log(naoExisto);
+} catch(err){// recebe o erro
+console.log("nao deu certo rapa"); 
+} 
+
+function soma( x,y){
+    if(typeof x!== 'number' || typeof y !== 'number'){
+        
+    }
+}
