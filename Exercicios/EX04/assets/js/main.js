87e74f7448329f79c595e8ab1ec093229e78f80a
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-06-24 15:57:03 -0300

    Estudos

--- Exercicios/EX04/assets/js/main.js
+++ Exercicios/EX04/assets/js/main.js
@@ -1,20 +1,55 @@
-function mostraHora(){
-    let data = new Date();
+// Selecionar os elementos do DOM
+const relogio = document.querySelector('.relogio');
+const iniciarBtn = document.querySelector('.Iniciar');
+const pausarBtn = document.querySelector('.Pausar');
+const zerarBtn = document.querySelector('.Zerar');
 
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false
-    })
- }
- 
- const timer = setInterval(function(){
-    console.log (mostraHora())}
-     , 1000);
+let timer;
+let segundos = 0;
+let pausado = true;
 
- setTimeout(function(){
-     clearInterval(timer);
- }, 3000);
- 
- 
- setTimeout(function()  {
-     console.log('Ola mundo!');
- }, 5000);
+function formatTime(seconds) {
+  const date = new Date(seconds * 1000);
+  return date.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'UTC'
+  });
+}
+
+function atualizarRelogio() {
+  relogio.textContent = formatTime(segundos);
+}
+
+function iniciarTimer() {
+  if (pausado) {
+    pausado = false;
+    relogio.classList.remove('pausado');
+    relogio.classList.add('despausado');
+    timer = setInterval(() => {
+      segundos++;
+      atualizarRelogio();
+    }, 1000);
+  }
+}
+
+function pausarTimer() {
+  if (!pausado) {
+    pausado = true;
+    clearInterval(timer);
+    relogio.classList.add('pausado');
+    relogio.classList.remove('despausado');
+  }
+}
+
+function zerarTimer() {
+  pausarTimer();
+  segundos = 0;
+  atualizarRelogio();
+}
+
+iniciarBtn.addEventListener('click', iniciarTimer);
+pausarBtn.addEventListener('click', pausarTimer);
+zerarBtn.addEventListener('click', zerarTimer);
+
+// Inicializar o display do relógio
+atualizarRelogio();
