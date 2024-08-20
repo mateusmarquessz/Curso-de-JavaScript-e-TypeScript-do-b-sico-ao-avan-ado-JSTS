3dd6f835025afa4cea688b95e4bd9784e1c3efa6
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-08-19 21:51:32 -0300

    Arrays

---
+++ Secao 4 Javascript Arrays (Avancado)/aula 54/index.js
@@ -0,0 +1,6 @@
+const nomes = ['Eduardo', 'Maria', 'Joana'];
+nomes[2] = 'Joao';
+delete nomes[2];
+console.log(nomes);
+
+//arrays
---
+++ Secao 4 Javascript Arrays (Avancado)/aula 55/index.js
@@ -0,0 +1,4 @@
+const nomes = ['Eduardo', 'Maria', 'Joana'];
+
+nomes.splice(4, 1);
+console.log(nomes);
